f7dd2e2bdfdf1946edb66df4d08055bdd68e8ab7
pocomane <none@mail.com>
2022-01-28 12:42:24 +0100

    Better quick help at calculators loading

--- calcjs_initial.js
+++ calcjs_initial.js
@@ -1,1 +1,7 @@
-2 + 2 // click Run or press Esc to run
+// Click Run or press ESC to evaluate the following expression:
+2 + 2
+
+// Multiple Javascript statements are supported too, e.g.:
+//   function fac(x){ if(x<=0){ return 1; }else{ return x*fac(x-1); }};
+//   a = fac(3);
+//   return Math.PI * fac(a);
--- calclua_initial.lua
+++ calclua_initial.lua
@@ -1,1 +1,7 @@
-2 + 2 -- click Run or press Esc to run
+-- Click Run or press ESC to evaluate the following expression:
+2 + 2
+
+-- Multiple lua statements are supported too, e.g.:
+--   function fac(x) return x<=0 and 1 or x*fac(x-1) end
+--   a = fac(3)
+--   b = pi * fac(a)
